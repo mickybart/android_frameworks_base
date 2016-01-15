410bd32fd0c4d3e923b6c2b7e7da66909b64bfa1
mickybart <mickybart@pygoscelis.org>
2016-01-15 05:38:12 -0500

    Lightbar managment (off, timeout, linked mode and adaptive brightness support)

--- services/core/java/com/android/server/lights/LightsManager.java
+++ services/core/java/com/android/server/lights/LightsManager.java
@@ -16,7 +16,13 @@
 
 package com.android.server.lights;
 
+import android.os.SystemProperties;
+
 public abstract class LightsManager {
+    public static final int LIGHT_ID_BUTTONS_DISABLE = 0;
+    public static final int LIGHT_ID_BUTTONS_TIMEOUT = 1;
+    public static final int LIGHT_ID_BUTTONS_LINKED = 2;
+
     public static final int LIGHT_ID_BACKLIGHT = 0;
     public static final int LIGHT_ID_KEYBOARD = 1;
     public static final int LIGHT_ID_BUTTONS = 2;
@@ -27,5 +33,57 @@ public abstract class LightsManager {
     public static final int LIGHT_ID_WIFI = 7;
     public static final int LIGHT_ID_COUNT = 8;
 
+    protected int mButtonsLightMode;
+    private int mColorBacklight = 0;
+    private final Object mLock = new Object();
+
+    public LightsManager() {
+        //Set the LIGHT_ID_BUTTONS mode
+        mButtonsLightMode = SystemProperties.getInt("persist.sys.lightbar_mode", LIGHT_ID_BUTTONS_TIMEOUT);
+        if (mButtonsLightMode < LIGHT_ID_BUTTONS_DISABLE || 
+                   mButtonsLightMode > LIGHT_ID_BUTTONS_LINKED) {
+            mButtonsLightMode = LIGHT_ID_BUTTONS_TIMEOUT;
+        }
+    }
+
     public abstract Light getLight(int id);
+
+    public void setBacklightBrightness(int brightness) {
+        synchronized (mLock) {
+            if (mColorBacklight != brightness) {
+                mColorBacklight = brightness;
+                getLight(LIGHT_ID_BACKLIGHT).setBrightness(brightness);
+
+                if (mButtonsLightMode == LIGHT_ID_BUTTONS_LINKED) {
+                    getLight(LIGHT_ID_BUTTONS).setBrightness(brightness);
+                } else if (mButtonsLightMode == LIGHT_ID_BUTTONS_TIMEOUT) {
+                    getLight(LIGHT_ID_BUTTONS).setBrightnessIfNotOff(brightness);
+                }
+            }
+        }
+    }
+
+    public void turnOnButtons() {
+        if (!isButtonsLightDisable()) {
+            synchronized (mLock) {
+                getLight(LIGHT_ID_BUTTONS).setBrightness(mColorBacklight);
+            }
+        }
+    }
+
+    public void turnOffButtons() {
+        if (!isButtonsLightDisable()) {
+            synchronized (mLock) {
+                getLight(LIGHT_ID_BUTTONS).turnOff();
+            }
+        }
+    }
+
+    protected boolean isButtonsLightDisable() {
+        return mButtonsLightMode == LIGHT_ID_BUTTONS_DISABLE;
+    }
+
+    public boolean isButtonsLightTimeout() {
+        return mButtonsLightMode == LIGHT_ID_BUTTONS_TIMEOUT;
+    }
 }
