f6805797427d0a08c629392d89329eb43644d3f9
mickybart <mickybart@pygoscelis.org>
2016-01-27 22:02:20 +0100

    hw keys: remap Camera key to launch Camera application

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -5053,8 +5053,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         switch (keyCode) {
             case KeyEvent.KEYCODE_VOLUME_DOWN:
             case KeyEvent.KEYCODE_VOLUME_UP:
-            case KeyEvent.KEYCODE_VOLUME_MUTE:
-            case KeyEvent.KEYCODE_CAMERA : {
+            case KeyEvent.KEYCODE_VOLUME_MUTE: {
                 if (mUseTvRouting) {
                     // On TVs volume keys never go to the foreground app
                     result &= ~ACTION_PASS_TO_USER;
@@ -5133,11 +5132,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                                     case KeyEvent.KEYCODE_VOLUME_UP:
                                         newKeyCode = KeyEvent.KEYCODE_MEDIA_NEXT;
                                         break;
-                                    case KeyEvent.KEYCODE_VOLUME_DOWN:
+                                    default:
                                         newKeyCode = KeyEvent.KEYCODE_MEDIA_PREVIOUS;
-                                        break;
-                                    default: // KeyEvent.KEYCODE_CAMERA
-                                        newKeyCode = KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE;
                                         break;
                                 }
                                 
@@ -5162,7 +5158,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
                     if (mUseTvRouting) {
                         dispatchDirectAudioEvent(event);
-                    } else if (mayChangeVolume && keyCode != KeyEvent.KEYCODE_CAMERA) {
+                    } else if (mayChangeVolume) {
                         // If we aren't passing to the user and no one else
                         // handled it send it to the session manager to
                         // figure out.
@@ -5174,6 +5170,20 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                                 .sendVolumeKeyEvent(newEvent, true);
                     }
                     break;
+                }
+                break;
+            }
+
+            case KeyEvent.KEYCODE_CAMERA : {
+                if (down) {
+                    Intent intent;
+                    if (keyguardActive) {
+                        intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA_SECURE);
+                    } else {
+                        intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+                    }
+                    isWakeKey = true;
+                    startActivityAsUser(intent, UserHandle.CURRENT_OR_SELF);
                 }
                 break;
             }
@@ -5373,7 +5383,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KeyEvent.KEYCODE_MEDIA_RECORD:
             case KeyEvent.KEYCODE_MEDIA_FAST_FORWARD:
             case KeyEvent.KEYCODE_MEDIA_AUDIO_TRACK:
-            case KeyEvent.KEYCODE_CAMERA:
                 return false;
         }
         return true;
