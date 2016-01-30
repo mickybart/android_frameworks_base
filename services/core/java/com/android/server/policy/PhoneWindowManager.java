d69c5c42ae171f53077671ef4b5648c4e555c172
mickybart <mickybart@pygoscelis.org>
2016-01-30 09:50:34 +0100

    hw keys: Camera: wakeup is not interactive early

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -5969,6 +5969,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
 
             case KeyEvent.KEYCODE_CAMERA : {
+                if (!interactive) {
+                    isWakeKey = true;
+                }
+
                 if (down) {
                     mKeysIsLongPress = false;
                     scheduleLongPressKeyEvent(event, KeyEvent.KEYCODE_CAMERA);
