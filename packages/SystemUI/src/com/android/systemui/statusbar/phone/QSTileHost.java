88bc2e490686ec9001533756eaf7c3895d806e48
mickybart <mickybart@pygoscelis.org>
2016-11-02 12:08:18 -0400

    Revert "SystemUI: Tile: Day/Night Mode"
    This reverts commit f25126b017207aebb570fc9891e3109b6dbd43f3.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -60,7 +60,6 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.LockscreenToggleTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
-import com.android.systemui.qs.tiles.ScreenColorTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.UserTile;
@@ -450,7 +449,6 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("network_adb")) return new AdbOverNetworkTile(this);
         else if (tileSpec.equals("caffeine")) return new CaffeineTile(this);
         else if (tileSpec.equals("immersive_mode")) return new ImmersiveModeTile(this);
-        else if (tileSpec.equals("screencolor")) return new ScreenColorTile(this);
         else if (tileSpec.equals("jackdetforce")) return new JackDetForceTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
